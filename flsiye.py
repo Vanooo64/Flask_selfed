36472141b884c106545eb8c41eb0a50b0ff3e118
Vano <glazglojon@gmail.com>
2024-12-27 18:58:06 +0200

    5 урок - Підключення зовнішніх ресурсів та робота з формами

--- flsiye.py
+++ flsiye.py
@@ -1,33 +1,40 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
-menu = ['Встановлення', 'Перший додаток', 'Зворотній звязок']
+menu = [{"name": "Встановлення", "url": "install-flask"},
+        {"name": "Перший додаток", "url": "first-app"},
+        {"name": "Зворотній звязок", "url": "contact"}]
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    print(url_for('index'))
     return render_template('index.html', menu=menu)
 
 @app.route("/about")
 def about():
-    print(url_for('about'))
     return render_template('about.html', title='Про сайт', menu=menu)
 
-@app.route("/profile/<username>")
-def profile(username, path):
-    return f"Користувач: {username}, {path}"
+@app.route("/contact", methods=['POST', 'GET'])
+def contact():
+    if request.method == "POST":
+        print(request.form)
 
+    return render_template('contact.html', title='Зворотній звязок', menu=menu)
 
-with app.test_request_context():
-    print(url_for('index'))
-    print(url_for('about'))
-    print(url_for('profile', username='ivan'))
+# @app.route("/profile/<username>")
+# def profile(username, path):
+#     return f"Користувач: {username}, {path}"
+#
+#
+# with app.test_request_context():
+#     print(url_for('index'))
+#     print(url_for('about'))
+#     print(url_for('profile', username='ivan'))
 
 
 # with app.test_request_context(): # створює тимчасовий контекст для тестування додатку без його запуску.
 #     print(url_for('about')) #Повертає URL маршруту about (/about) і друкує його в консоль.
 
-# if __name__ == "__main__": #перевіряє, чи скрипт виконується напряму.
-#     app.run(debug=True) #запускає сервер Flask у режимі розробки (включає автоматичний перезапуск і докладний відладочний вивід у випадку помилок).
+if __name__ == "__main__": #перевіряє, чи скрипт виконується напряму.
+    app.run(debug=True) #запускає сервер Flask у режимі розробки (включає автоматичний перезапуск і докладний відладочний вивід у випадку помилок).
